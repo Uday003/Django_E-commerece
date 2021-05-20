7eee726014f224e5594d11b27b138af3aa5ac50b
Uday003 <83628702+Uday003@users.noreply.github.com>
2021-05-21 01:11:14 +0530

    Update views.py

--- django_1/shoppe/views.py
+++ django_1/shoppe/views.py
@@ -8,3 +8,11 @@ def home(request):
     return render(request,'shoppe/home.html',context)
 def about(request):
     return render(request,'shoppe/about.html',{'title':'E commerce'})
+def electronics(request):
+    return render(request,'shoppe/electronics.html')
+def fashion(request):
+    return render(request,'shoppe/fashion.html',)
+def grocery(request):
+    return render(request,'shoppe/grocery.html',)    
+def successful_buy(request):
+    return render(request,'shoppe/successful_buy.html',)
