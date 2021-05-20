51be42c0665442a7571e017c77645a5c11628158
Uday003 <83628702+Uday003@users.noreply.github.com>
2021-05-21 01:10:12 +0530

    Update urls.py

--- django_1/shoppe/urls.py
+++ django_1/shoppe/urls.py
@@ -3,4 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.home,name='Shoppe-Home'),
     path('about/',views.about,name='Shoppe-About'),
+    path('electronics/',views.electronics,name='electronics'),
+    path('fashion/',views.fashion,name='fashion'),
+    path('grocery/',views.grocery,name='grocery'),
+    path('sucessful_buy/',views.successful_buy,name='success'),
 ]
