88086bd8be1f32c9406195652217e4ad3ec3385e
Uday kumar Gangapuram <83628702+Uday003@users.noreply.github.com>
2021-05-21 12:43:58 +0530

    Update views.py

--- django_1/shoppe/views.py
+++ django_1/shoppe/views.py
@@ -8,13 +8,18 @@ def home(request):
         'posts':Post.objects.all()
     }
     return render(request,'shoppe/home.html',context)
+@login_required
 def about(request):
     return render(request,'shoppe/about.html',{'title':'E commerce'})
+@login_required
 def electronics(request):
     return render(request,'shoppe/electronics.html')
+@login_required
 def fashion(request):
     return render(request,'shoppe/fashion.html',)
+@login_required
 def grocery(request):
-    return render(request,'shoppe/grocery.html',)    
+    return render(request,'shoppe/grocery.html',)  
+@login_required
 def successful_buy(request):
     return render(request,'shoppe/successful_buy.html',)
